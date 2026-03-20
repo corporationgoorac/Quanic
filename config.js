eda91fe96b6ac63b4076514eb6fecdf2f5aa6aff
corporationgoorac <corporationgoorac@gmail.com>
2026-03-20 13:38:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
-// Add the Auth and Firestore SDKs needed for Quanic
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBcqRDG1k2zqQrfLGJa1qbz83B_IlffbtU",
   authDomain: "quanic-goorac.firebaseapp.com",
@@ -17,16 +12,13 @@ const firebaseConfig = {
   measurementId: "G-SE4ZTDFE1L"
 };
 
-// 1. Initialize Firebase App
+// Initialize Services
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// 2. Initialize Firebase Authentication
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-
-// 3. Initialize Cloud Firestore (Database)
 const db = getFirestore(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider(); // For easy Google Login
 
-// 4. Export these tools so your other HTML pages can use them
-export { app, auth, googleProvider, db, analytics };
+// ImgBB API Key for Image Uploads
+const IMGBB_API_KEY = "Ec521beb5111f54fc727ee473dea38be";
+
+export { db, auth, googleProvider, IMGBB_API_KEY };
